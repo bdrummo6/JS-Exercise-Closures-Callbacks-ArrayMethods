e1623abc51272766f7d7c3bc23ae9d62872a4f62
Bradley Drummonds <47236494+bdrummo6@users.noreply.github.com>
2020-01-16 23:45:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -242,8 +242,16 @@ function getRunnersByTShirtSize(runners, tShirtSize) {
  * @param runners array of runners like the one inside the /data/runners.js file.
  * @returns a number which is the sum of the donations by all runners.
 */
-function tallyUpDonations(/* CODE HERE */) {
-  /* CODE HERE */
+function tallyUpDonations(runners) {
+  const donationList = runners.map((runner) => {
+    return runner.donation;
+  });
+
+  const donations = donationList.reduce((total, item) => {
+    return total += item;
+  }, 0);
+
+  return donations;
 }
 
 /////////////// CLOSURES ///////////////
